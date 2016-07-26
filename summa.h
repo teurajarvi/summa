e7eef681a7f81554bc1b4a094a0df0a0a141f140
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2016-07-26 13:56:26 +0300

    Create summa.h
    Initial version of .h header file for program calculating the sum of 2 numbers (language Finnish)

---
+++ summa.h
@@ -0,0 +1,11 @@
+/* oma header: summa.h */
+
+/* muuttujat */
+int luku_lopputulos = 0;
+
+/* tyypit */
+#define LUKUVIRHE 9999
+
+/* funktion määritelmät */
+void tarkistus( );
+int laske_yhteen( );
